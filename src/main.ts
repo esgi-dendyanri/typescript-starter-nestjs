ce48fdd3ddff680bec408961820ae69440fb31ea
Esgi Dendyanri <dend.esgi@gmail.com>
2022-06-05 00:23:48 +0700

    deploy to heroku

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const app = NestFactory.create(AppModule);
+// const app = NestFactory.create(AppModule);
 
-console.log("XXXXX")
-module.exports = app;
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   await app.listen(3000);
-// }
-// bootstrap();
+// console.log("XXXXX")
+// module.exports = app;
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
