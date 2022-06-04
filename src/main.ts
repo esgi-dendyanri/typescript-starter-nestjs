0e6f634bc729d5e266ca8fd785e62f0646adc5cf
Esgi Dendyanri <dend.esgi@gmail.com>
2022-06-04 23:16:53 +0700

    fix runner

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const app = NestFactory.create(AppModule);
+
+console.log("XXXXX")
+module.exports = app;
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+// }
+// bootstrap();
--- vercel-runner.js
+++ vercel-runner.js
@@ -1,12 +1,29 @@
 var exec = require('child_process').exec;
 
-exec(
-    'npm run build & npm run start:prod',
-    function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-            console.log('exec error: ' + error);
-        }
-    }
-);
+async function executeCommand(command) {
+    return new Promise((resolve, reject) => {
+        console.log("COMMAND", command);
+
+        exec(
+            command,
+            function (error, stdout, stderr) {
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
+                if (error !== null) {
+                    console.log('exec error: ' + error);
+                }
+
+                resolve();
+            }
+        );
+    })
+}
+
+async function main() {
+    await executeCommand("npm install")
+    await executeCommand("npm run build")
+
+    return require("./dist/main");
+}
+
+module.exports = main();
